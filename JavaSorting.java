d657c7eb607f8ebbdf1d3865d87af7733a9cc687
Strikker0 <97169587+Strikker0@users.noreply.github.com>
2022-10-16 00:06:12 +0530

    Some of the most common sorting algorithms

--- JavaSorting.java
+++ JavaSorting.java
@@ -1,1 +1,12 @@
 // write all the names of sorting algo
+Some of the most common sorting algorithms are:
+
+Selection sort
+Bubble sort
+Insertion sort
+Merge sort
+Quick sort
+Heap sort
+Counting sort
+Radix sort
+Bucket sort
